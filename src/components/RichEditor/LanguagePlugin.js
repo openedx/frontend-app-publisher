2fff5edae3db2b0654c1b31ab0518e42883167e9
Karen Gu <karengu@C02ZW2K4LVDM.tld>
2020-01-22 14:12:28 -0500

    cleanup

--- src/components/RichEditor/LanguagePlugin.js
+++ src/components/RichEditor/LanguagePlugin.js
@@ -2,87 +2,86 @@ import tinymce from 'tinymce/tinymce.min';
 import { BROWSER_DEFAULT, languages } from './editorConstants';
 
 tinymce.PluginManager.add('language', (editor) => {
-  const replaceText = (newText) => {
+  const replaceText = (lang) => {
+    // Depending on the current location of the cursor, replace the current node with
+    // the HTML in `lang`
+    const newSpanText = lang === BROWSER_DEFAULT ? '<span id="new_span">&#65279</span>' : `<span lang=${lang} id="new_span">&#65279</span>`;
     const selectedNode = editor.selection.getNode();
     if (selectedNode.nodeName === 'SPAN') {
       if (!selectedNode.innerText.trim()) {
-        editor.dom.setOuterHTML(selectedNode, newText);
+        // inner text consists of only whitespace, so replace entire span to reduce nesting
+        editor.dom.setOuterHTML(selectedNode, newSpanText);
       } else {
-        editor.dom.setOuterHTML(selectedNode, `${selectedNode.outerHTML}${newText}`);
+        // inner text does not consist of only whitespace, so append new span to existing HTML
+        editor.dom.setOuterHTML(selectedNode, `${selectedNode.outerHTML}${newSpanText}`);
       }
-    } else if (!selectedNode.innerText.trim() && selectedNode.innerText !== ' ') {
-      selectedNode.innerHTML = newText;
     } else if (selectedNode.nodeName === 'P') {
-      selectedNode.innerHTML += newText;
+      // selection is part of a bigger paragraph, so append the new span to existing HTML
+      selectedNode.innerHTML += newSpanText;
     } else if (selectedNode.nodeName === 'BR') {
-      editor.dom.setOuterHTML(selectedNode, newText);
+      // text consists of only whitespace, so replace all of it to avoid adding extra whitespace
+      editor.dom.setOuterHTML(selectedNode, newSpanText);
     } else {
-      editor.dom.setOuterHTML(selectedNode, `${selectedNode.outerHTML}${newText}`);
+      // conservatively assume that the selection contains more HTML to the left
+      editor.dom.setOuterHTML(selectedNode, `${selectedNode.outerHTML}${newSpanText}`);
     }
     const newSpan = editor.dom.get('new_span');
     editor.selection.select(newSpan);
-    editor.selection.collapse(false);
+    editor.selection.collapse(false); // moves cursor to end of selection
     newSpan.removeAttribute('id');
-  };
-  const resetLanguage = () => {
-    replaceText('<span id="new_span">&#65279</span>');
   };
 
   const openDialog = (buttonApi) => {
-    const selectedNode = editor.selection.getNode();
-    const currentLang = selectedNode.lang ? selectedNode.lang : BROWSER_DEFAULT;
-    return editor.windowManager.open({
-      title: 'Language plugin',
-      body: {
-        type: 'panel',
-        items: [
+    if (!editor.selection.isCollapsed()) {
+      editor.notificationManager.open({
+        text: 'You cannot set the language of a selection. Change the language first and then continue typing.',
+        type: 'error',
+      });
+    } else {
+      const selectedNode = editor.selection.getNode();
+      const currentLang = selectedNode.lang ? selectedNode.lang : BROWSER_DEFAULT;
+      editor.windowManager.open({
+        title: 'Language plugin',
+        body: {
+          type: 'panel',
+          items: [
+            {
+              type: 'htmlpanel',
+              html: `<div>Current language: ${languages[currentLang].nativeName}</div>`,
+            },
+            {
+              type: 'selectbox',
+              name: 'language',
+              label: 'Language',
+              items: Object.keys(languages).map(lang => ({
+                value: lang,
+                text: languages[lang].nativeName,
+              })),
+            },
+          ],
+        },
+        buttons: [
           {
-            type: 'htmlpanel', // component type
-            html: `<div>Current language: ${languages[currentLang].nativeName}</div>`,
+            type: 'cancel',
+            text: 'Close',
           },
           {
-            type: 'selectbox',
-            name: 'language',
-            label: 'Language',
-            items: Object.keys(languages).map(lang => ({
-              value: lang,
-              text: languages[lang].nativeName,
-            })),
+            type: 'submit',
+            text: 'Save',
+            primary: true,
           },
         ],
-      },
-      buttons: [
-        {
-          type: 'cancel',
-          text: 'Close',
-        },
-        {
-          type: 'submit',
-          text: 'Save',
-          primary: true,
-        },
-      ],
-      onSubmit(api) {
-        const data = api.getData();
-        editor.focus();
-        if (!editor.selection.isCollapsed()) {
-          editor.notificationManager.open({
-            text: 'You cannot set the language of a selection. Change the language first and then continue typing.',
-            type: 'error',
+        onSubmit(api) {
+          const data = api.getData();
+          editor.focus();
+          editor.undoManager.transact(() => {
+            replaceText(data.language);
+            buttonApi.setActive(data.language !== BROWSER_DEFAULT);
           });
           api.close();
-          return;
-        }
-        if (data.language !== BROWSER_DEFAULT) {
-          const dummySpan = `<span lang=${data.language} id="new_span">&#65279</span>`;
-          replaceText(dummySpan);
-        } else if (selectedNode.lang) {
-          resetLanguage(selectedNode);
-        }
-        buttonApi.setActive(data.language !== BROWSER_DEFAULT);
-        api.close();
-      },
-    });
+        },
+      });
+    }
   };
 
   editor.ui.registry.addToggleButton('language', {
@@ -95,24 +94,23 @@ tinymce.PluginManager.add('language', (editor) => {
         const selectedNode = editor.selection.getNode();
         const currentLang = selectedNode.lang ? selectedNode.lang : BROWSER_DEFAULT;
         if (currentLang !== BROWSER_DEFAULT) {
-          resetLanguage();
-          buttonApi.setActive(false);
+          editor.undoManager.transact(() => {
+            replaceText(BROWSER_DEFAULT);
+            buttonApi.setActive(false);
+          });
         }
       });
       const updateCurrentLanguage = () => {
-        const selectedNode = editor.selection.getNode();
-        if (selectedNode.lang) {
-          buttonApi.setActive(true);
-        } else {
-          buttonApi.setActive(false);
-        }
+        if (editor.selection.getNode().lang) buttonApi.setActive(true);
+        else buttonApi.setActive(false);
       };
-      editor.on('keyup', () => {
-        updateCurrentLanguage();
-      });
-      editor.on('click', () => {
-        updateCurrentLanguage();
-      });
+      editor.on('keyup', updateCurrentLanguage);
+      editor.on('click', updateCurrentLanguage);
+      return () => {
+        // remove event listeners on teardown
+        editor.off('keyup', updateCurrentLanguage);
+        editor.off('click', updateCurrentLanguage);
+      };
     },
   });
 
