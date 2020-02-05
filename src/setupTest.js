835cdc5a2a52f3c0c3402232c04f3d9e0590b6c1
Nick <ndalfonso@edx.org>
2020-02-05 14:03:56 -0500

    Revert "Update dependency tinymce to v5.1.4 [SECURITY] (#381)" (#383)
    This reverts commit b994a590d24460e0595ad296af8fa9946c62f4b8.

--- src/setupTest.js
+++ src/setupTest.js
@@ -8,21 +8,3 @@ Enzyme.configure({ adapter: new Adapter() });
 // These configuration values are usually set in webpack's EnvironmentPlugin however
 // Jest does not use webpack so we need to set these so for testing
 process.env.DISCOVERY_API_BASE_URL = 'http://localhost:18381';
-
-// We need this here because there is code which uses a method(s) which JSDOM has not
-// implemented yet. To fix this, the following mocks matchMedia so that all tests
-// execute properly. More info here:
-// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
