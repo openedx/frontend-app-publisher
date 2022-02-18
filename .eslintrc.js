2b939f4c119a64ab633a64412eba213792ecc495
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2022-02-18 14:31:24 +0500

    fix: replace babel-eslint with @babel/eslint-parser
    babel-eslinnt has been moved inside babel monorepo and renamed
    to @babel/eslint-parser. Remove this pin once it has been replaced
    inside fronted-build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
 const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('eslint');
+module.exports = createConfig('eslint', {
+    parser: "@babel/eslint-parser", // TODO: remove this once babel-eslint is replaced with @babel/eslint-parser in frontend-build
+});
