7548366093facc18ad1467728faffc4894788699
Kira Miller <kira.miller15@yahoo.com>
2023-12-20 21:17:09 +0000

    fix: test changes

--- src/components/SidePanes/CatalogInclusionPane.test.jsx
+++ src/components/SidePanes/CatalogInclusionPane.test.jsx
@@ -2,46 +2,61 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import CatalogInclusionPane from './CatalogInclusionPane';
+import DiscoveryDataApiService from '../../data/services/DiscoveryDataApiService';
 
 describe('CatalogInclusionPane', () => {
   const mockUuid = 'test-enterprise-id';
-  const mockInclusion = false;
+  const mockSubInclusion = false;
+  const mockOrgInclusion = true;
+  const spy = jest.spyOn(DiscoveryDataApiService, 'editCourse');
 
   it('correct toggle behavior', () => {
     const wrapper = mount(<CatalogInclusionPane
       courseUuid={mockUuid}
-      subInclusion={mockInclusion}
+      subInclusion={mockSubInclusion}
       draftStatuses={['published']}
-      orgInclusion
+      orgInclusion={mockOrgInclusion}
     />);
     wrapper.find(CatalogInclusionPane);
     const title = wrapper.find('Enterprise Subscriptions');
     expect(title);
     const toggle = wrapper.find('.pgn__form-switch-input');
     toggle.simulate('change', { target: { checked: false } });
-    expect(toggle.props().checked).toBe(false);
+    expect(spy).toBeCalledWith(
+      {
+        draft: false,
+        enterprise_subscription_inclusion: !mockSubInclusion,
+        uuid: 'test-enterprise-id',
+      },
+    );
   });
 
   it('allow course runs who have been reviewed', () => {
     const wrapper = mount(<CatalogInclusionPane
       courseUuid={mockUuid}
-      subInclusion={mockInclusion}
+      subInclusion={mockSubInclusion}
       draftStatuses={['reviewed']}
-      orgInclusion
+      orgInclusion={mockOrgInclusion}
     />);
     wrapper.find(CatalogInclusionPane);
     const title = wrapper.find('Enterprise Subscriptions');
     expect(title);
     const toggle = wrapper.find('.pgn__form-switch-input');
     toggle.simulate('change', { target: { checked: false } });
-    expect(toggle.props().checked).toBe(false);
+    expect(spy).toBeCalledWith(
+      {
+        draft: false,
+        enterprise_subscription_inclusion: !mockSubInclusion,
+        uuid: 'test-enterprise-id',
+      },
+    );
   });
-
   it('toggle disabled when org is false', () => {
     const wrapper = mount(<CatalogInclusionPane
       courseUuid={mockUuid}
-      subInclusion={mockInclusion}
+      subInclusion={mockSubInclusion}
       draftStatuses={['published']}
+      orgInclusion={false}
     />);
     // org not included helper text
     expect(wrapper.find('.text-gray-300')).toHaveLength(1);
@@ -49,9 +64,9 @@ describe('CatalogInclusionPane', () => {
   it('toggle blocked in review status', () => {
     const wrapper = mount(<CatalogInclusionPane
       courseUuid={mockUuid}
-      subInclusion={mockInclusion}
+      subInclusion={mockSubInclusion}
       draftStatuses={['review_by_internal']}
-      orgInclusion
+      orgInclusion={mockOrgInclusion}
     />);
     const toggle = wrapper.find('.pgn__form-switch-input');
     toggle.simulate('change', { target: { checked: false } });
