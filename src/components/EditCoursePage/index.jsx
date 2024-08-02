b8be8862ced2aaeb45fbd4187b1319c9dd34b7a0
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2024-08-02 11:48:29 +0500

    feat: updated major versions for frontend-platform & paragon (#947)

--- src/components/EditCoursePage/index.jsx
+++ src/components/EditCoursePage/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { Alert } from '@edx/paragon';
+import { Alert } from '@openedx/paragon';
 
 import EditCourseForm from './EditCourseForm';
 import PageContainer from '../PageContainer';
