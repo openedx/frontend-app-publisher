c4fee9e0bfd5180940d213cab6d4a686b9a07383
Hunia Fatima <hunia.fatima@arbisoft.com>
2025-04-11 14:59:36 +0500

    chore: update tests in pill and help text

--- src/components/CourseTable/CourseTable.test.jsx
+++ src/components/CourseTable/CourseTable.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CourseTable from './index';
-import {MemoryRouter} from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
 describe('CourseTable', () => {
   it('shows a table', () => {
