5857e46717f6b1138d881139e198273575c0412f
Hunia Fatima <hunia.fatima@arbisoft.com>
2025-04-21 13:36:53 +0500

    chore: update snapshots

--- src/components/Collaborator/renderSuggestion.jsx
+++ src/components/Collaborator/renderSuggestion.jsx
@@ -3,7 +3,7 @@ import NewInstructorImage from '../../../assets/new-instructor-80.png';
 
 function renderSuggestion(suggestion) {
   return (
-    <div className="d-flex flex-row m-1 p-1">
+    <div data-testid="list-field-suggestion" className="d-flex flex-row m-1 p-1">
       <div className="m-1 p-1 w-25">
         <img
           src={suggestion.image_url || NewInstructorImage}
