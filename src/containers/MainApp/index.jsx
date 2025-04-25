65b09939ee8a95d56b403712b1982502786c7d9c
Syed Muhammad Dawoud Sheraz Ali <dawoud.sheraz@gmail.com>
2025-04-25 22:35:42 +0500

    temp: commit changes

--- src/containers/MainApp/index.jsx
+++ src/containers/MainApp/index.jsx
@@ -1,7 +1,7 @@
 import 'core-js';
 import 'regenerator-runtime/runtime';
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Footer from '@edx/frontend-component-footer';
 
 import '../../sass/App.scss';
@@ -18,50 +18,48 @@ import EditCourse from '../EditCourse';
 import SitewideBanner from '../../components/SitewideBanner';
 
 const MainApp = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      {/* Only display the banner if there is content to display. */}
-      {process.env.SITEWIDE_BANNER_CONTENT && (
-        <SitewideBanner
-          message={process.env.SITEWIDE_BANNER_CONTENT}
-          type="warning"
-          dismissible
-          cookieName="publisherSiteWideBannerDismissed"
-          cookieExpiryDays={30}
+  <div>
+    <Header />
+    {/* Only display the banner if there is content to display. */}
+    {process.env.SITEWIDE_BANNER_CONTENT && (
+    <SitewideBanner
+      message={process.env.SITEWIDE_BANNER_CONTENT}
+      type="warning"
+      dismissible
+      cookieName="publisherSiteWideBannerDismissed"
+      cookieExpiryDays={30}
+    />
+    )}
+    <main>
+      <Routes>
+        <Route path="/course-runs/:courseRunKey" element={<CourseRunRedirectComponent />} />
+        <Route
+          path="/courses/new"
+          element={<CreateCourse />}
         />
-      )}
-      <main>
-        <Routes>
-          <Route path="/course-runs/:courseRunKey" element={<CourseRunRedirectComponent />} />
-          <Route
-            path="/courses/new"
-            element={<CreateCourse />}
-          />
-          <Route path="/courses/:uuid/rerun" element={<CreateCourseRun />} />
-          <Route
-            path="/instructors/new"
-            element={<CreateStaffer />}
-          />
-          <Route path="/instructors/:uuid" element={<EditStaffer />} />
-          <Route
-            path="/collaborators/new"
-            element={<CreateCollaborator />}
-          />
-          <Route
-            path="/collaborators/:uuid"
-            element={<EditCollaborator />}
-          />
-          <Route path="/courses/:uuid" element={<EditCourse />} />
-          <Route
-            path="/"
-            element={<CourseDashboard />}
-          />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  </BrowserRouter>
+        <Route path="/courses/:uuid/rerun" element={<CreateCourseRun />} />
+        <Route
+          path="/instructors/new"
+          element={<CreateStaffer />}
+        />
+        <Route path="/instructors/:uuid" element={<EditStaffer />} />
+        <Route
+          path="/collaborators/new"
+          element={<CreateCollaborator />}
+        />
+        <Route
+          path="/collaborators/:uuid"
+          element={<EditCollaborator />}
+        />
+        <Route path="/courses/:uuid" element={<EditCourse />} />
+        <Route
+          path="/"
+          element={<CourseDashboard />}
+        />
+      </Routes>
+    </main>
+    <Footer />
+  </div>
 );
 
 export default MainApp;
