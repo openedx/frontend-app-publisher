3151b1def7edae34b3957b9b4ec8221f2aecf01a
Ali Nawaz <alinawaz2000@hotmail.com>
2025-05-26 17:05:20 +0500

    chore: cleanup end

--- src/components/RichEditor/RichEditor.test.jsx
+++ src/components/RichEditor/RichEditor.test.jsx
@@ -63,43 +63,26 @@ describe('RichEditor', () => {
 
   it('change handlers are called', async () => {
     const onChange = jest.fn();
-    const mockHandleOnChange = jest.spyOn(RichEditor.prototype, 'handleOnChange');
+    const mockHandleEditorChange = jest.spyOn(RichEditor.prototype, 'handleEditorChange');
 
     const {container} = render(<RichEditor
       label="Rich Text Editor Test"
       id="rich-text-editor-test"
       input={{
-        value: 'St',
+        value: 'Start',
         onChange,
       }}
       maxChars={500}
       meta={{ touched: true, error: 'Required' }}
     />);
-
     
-    // const user = await userEvent.setup();
+    // Initial render
     await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce')).toBeInTheDocument());
+    expect(await screen.findByText('Recommended character limit (including spaces) is 500. 495 characters remaining.')).toBeInTheDocument();
+    await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce').querySelector('p').textContent).toBe('Start'));
+
     
-    // Assert 0 and null
-    expect(await screen.findByText('Recommended character limit (including spaces) is 500. 498 characters remaining.')).toBeInTheDocument();
-    await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce').querySelector('p').textContent).toBe('St'));
-
-
-    // Fire change, assert onEditorChange and onChange called with right props
-  
-    const input = container.querySelector('iframe').contentDocument.querySelector('#tinymce')
-    // const input = container.querySelector('textarea')
-    
-    console.error('OH', input);
-
-    fireEvent.input(input, {target: {innerHTML: '<p>EDRDD</p>'}});
-    // await userEvent.keyboard('My boi')
-    
-    
-    // await userEvent.type(input, 'Please');
-
-    await new Promise(res => setTimeout(res, 3000));
-
+    // JSDOM does not support these, so stub them out
     container.querySelector('iframe').contentWindow.Range.prototype.getClientRects = () => ({
       item: () => null,
       length: 0,
@@ -112,30 +95,17 @@ describe('RichEditor', () => {
       left:20,
       right: 40
     });
-    // container.querySelector('iframe').contentWindow.HTMLElement.prototype.getBoundingClientRect = () => ({
-    //   top: 54,
-    //   bottom: 86,
-    //   left:20,
-    //   right: 40
-    // });
 
-    // fireEvent.change(input);
+    // Fire change, assert onEditorChange and onChange called with right props
+    const input = container.querySelector('iframe').contentDocument.querySelector('#tinymce')
+    fireEvent.keyUp(input, {target: {innerHTML: '<p>Hello World!</p>'}});
 
-    // await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce').querySelector('p').textContent).toBe('EDRDD'));
-    
-    // window.HTMLElement.prototype.getClientRects = () => [];
-    // container.querySelector('iframe').contentWindow.HTMLElement.prototype.getClientRects = () => [];
-    fireEvent.keyUp(input)
+    expect(onChange).toHaveBeenLastCalledWith('<p>Hello World!</p>');
+    await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce').querySelector('p').textContent).toBe('Hello World!'));
 
-    // editorComponent.prop('onEditorChange')('Hello, text', mockEditor);
+    expect(mockHandleEditorChange).toHaveBeenLastCalledWith('<p>Hello World!</p>', expect.anything());
+    expect(await screen.findByText('Recommended character limit (including spaces) is 500. 488 characters remaining.')).toBeInTheDocument();
 
-    expect(onChange).toHaveBeenLastCalledWith('<p>EDRDD</p>');
-    await waitFor(() => expect(container.querySelector('iframe').contentDocument.querySelector('#tinymce').querySelector('p').textContent).toBe('EDRDD'));
-
-    expect(mockHandleOnChange).toHaveBeenCalledTimes(1);
-
-    // expect(instance.state.charCount).toEqual(11);
-    // expect(instance.state.value).toEqual('Hello, text');
   });
 
   it('componentDidUpdate updates state', async () => {
@@ -143,38 +113,28 @@ describe('RichEditor', () => {
       label="Rich Text Editor Test"
       id="rich-text-editor-test"
       input={{
-        value: "This is amazing",
+        value: null,
         onChange: () => null,
       }}
       maxChars={500}
       meta={{ touched: true, error: 'Required' }}
     />);
-
-    
-    let iframe = await container.querySelector('iframe');
-
-    console.error(iframe, 'POO')
-    
-    console.error(iframe.contentDocument.readyState, 'PAPA')
-    
-    await waitFor(() => expect(iframe.contentDocument.querySelector('#tinymce').querySelector('p')).toBeInTheDocument());
-
-    console.error(iframe.contentDocument.querySelector('#tinymce').querySelector('p').textContent, 'FINALE');
 
     rerender(<RichEditor
       label="Rich Text Editor Test"
       id="rich-text-editor-test"
       input={{
-        value: "WOAH HOAH",
+        value: "New Value",
         onChange: () => null,
       }}
       maxChars={500}
       meta={{ touched: true, error: 'Required' }}
     />);
 
-
-    console.error(iframe.contentDocument.querySelector('#tinymce').querySelector('p').textContent, 'DINALE');
-
-
+    let iframe = container.querySelector('iframe');
+    await waitFor(() => expect(iframe.contentDocument.querySelector('p')).toBeInTheDocument());
+    
+    let tinymceContent = within(iframe.contentDocument.querySelector('p')).getByText('New Value');
+    expect(tinymceContent).toBeInTheDocument();
   });
 });
